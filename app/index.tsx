7b2bd98e216f9957abc505ad8a3ab20383dfe47a
Bartosz <bartoszkow03@gmail.com>
2024-10-02 17:41:51 +0200

    Added index screen with basic styles

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,80 @@
-import { Text, View } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import { TextInput } from "react-native-paper";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.innerContainer}>
+        <Text>
+          DO I<Text>T</Text>
+        </Text>
+        <Image
+          style={styles.logo}
+          source={require("/PROJEKTY/todo/Todo/assets/images/logo2.png")}
+        />
+        <Text style={styles.title}>Getting started</Text>
+        <TextInput
+          style={styles.textInput}
+          label={"Your Name"}
+          mode="outlined"
+          placeholder="Enter Your Name"
+          right={<TextInput.Affix text="/14" />}
+        ></TextInput>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Save Name</Text>
+        </TouchableOpacity>
+        <Text style={styles.subTitle}>
+          Start taking your notes and never forget anything!
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  innerContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textInput: {
+    width: "80%",
+    marginBottom: 16,
+  },
+  button: {
+    backgroundColor: "#79EA99",
+    padding: 10,
+    borderRadius: 5,
+    alignItems: "center",
+    width: "80%",
+    marginBottom: 16,
+  },
+  logo: {
+    width: 201,
+    height: 201,
+    marginVertical: 20,
+  },
+  title: {
+    fontSize: 30,
+    color: "#1F2937",
+    textAlign: "left",
+  },
+  subTitle: {
+    fontSize: 19,
+    color: "#1f2937",
+  },
+  buttonText: {
+    fontWeight: "bold",
+    color: "#1F2937",
+  },
+});
